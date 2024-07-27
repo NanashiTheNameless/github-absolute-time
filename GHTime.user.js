c85572ab3626c88eb5474ed45aab88246b43076b
Nanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2024-07-27 15:12:02 -0500

    Add files via upload

---
+++ GHTime.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name Github: always show absolute times
+// @version 1
+// @namespace namelessnanashi.dev
+// @updateURL https://github.com/NanashiTheNameless/github-absolute-time/GHTime.user.js
+// @downloadURL https://github.com/NanashiTheNameless/github-absolute-time/GHTime.user.js
+// @supportURL https://github.com/NanashiTheNameless/github-absolute-time
+// @copyright https://github.com/NanashiTheNameless/github-absolute-time/license.md
+// @description Github: always show absolute times
+// @author NamelessNanashi
+// @run-at document-end
+// @grant none
+// @match *://github.com/*
+// ==/UserScript==
+
+(function() { var style = document.createElement('style'); document.head.appendChild(style); var sheet = style.sheet; sheet.addRule('time-ago:before,relative-time:before', 'content: attr(title);display: block;font-size: 0.5rem;'); })()
