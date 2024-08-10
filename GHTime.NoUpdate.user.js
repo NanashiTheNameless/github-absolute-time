16a8fd1c30608fd26d85e72264c2c3380b08cea8
Nanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2024-08-09 21:00:20 -0500

    add an icon

--- GHTime.NoUpdate.user.js
+++ GHTime.NoUpdate.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name Github: always show absolute times
-// @version 1.1
+// @version 1.2
 // @namespace namelessnanashi.dev
 // @supportURL https://github.com/NanashiTheNameless/github-absolute-time/issues
 // @copyright https://github.com/NanashiTheNameless/github-absolute-time/license.md
 // @description Github: always show absolute times
 // @author NamelessNanashi
+// @icon https://github.githubassets.com/favicons/favicon.svg
 // @run-at document-end
 // @grant none
 // @match *://github.com/*
--- GHTime.user.js
+++ GHTime.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Github: always show absolute times
-// @version 1.1
+// @version 1.2
 // @namespace namelessnanashi.dev
 // @updateURL https://github.com/NanashiTheNameless/github-absolute-time/raw/main/GHTime.user.js
 // @downloadURL https://github.com/NanashiTheNameless/github-absolute-time/raw/main/GHTime.user.js
@@ -8,6 +8,7 @@
 // @copyright https://github.com/NanashiTheNameless/github-absolute-time/license.md
 // @description Github: always show absolute times
 // @author NamelessNanashi
+// @icon https://github.githubassets.com/favicons/favicon.svg
 // @run-at document-end
 // @grant none
 // @match *://github.com/*
